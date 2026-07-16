3f6b2f423df94871ee0801044da339a0465c1816
Muhammad Afnan <56377@students.riphah.edu.pk>
2026-07-16 17:26:02 +0500

    Restore footer Contact to mailto and remove /contact page per user request

--- constants/index.ts
+++ constants/index.ts
@@ -270,7 +270,7 @@ export const FOOTER_DATA = [
       {
         name: "Contact Me",
         icon: null,
-        link: "/contact",
+        link: "mailto:afnanazfar15@gmail.com",
       },
     ],
   },
